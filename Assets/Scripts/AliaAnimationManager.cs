f66519c5381b25b6e8fe7944408137078ac39d0d
michael.bowen <michael.bowen@mymail.champlain.edu>
2022-11-29 12:29:35 -0500

    Added additional movement conditions for moving around Added painting textures to terrain

--- Assets/Scripts/AliaAnimationManager.cs
+++ Assets/Scripts/AliaAnimationManager.cs
@@ -18,7 +18,13 @@ public class AliaAnimationManager : MonoBehaviour
     [SerializeField] private AliaMovement _aliaMovement;
 
     [SerializeField] private float speed;
+    [SerializeField] private float slowSpeed = 1;
+    
     [SerializeField] private float forwardSpeed;
+    [SerializeField] private float mediumSpeed = 3;
+    [SerializeField] private float inputThreshold = 0.1f;
+
+    private Rigidbody _aliaRigidbody;
     
     // Start is called before the first frame update
     void Awake()
@@ -29,14 +35,16 @@ public class AliaAnimationManager : MonoBehaviour
         _aliaMovement.LandEvent += OnLand;
         _aliaMovement.TakeoffEvent += OnTakeOff;
 
+        _aliaRigidbody = _aliaMovement.gameObject.GetComponent<Rigidbody>();
+
     }
 
     void OnLand(object sender, EventArgs e)
     {
-        _propFL.SetFloat("Speed", 0);
-        _propFR.SetFloat("Speed", 0);
-        _propRL.SetFloat("Speed", 0);
-        _propRR.SetFloat("Speed", 0);
+        _propFL.SetFloat("Speed", slowSpeed);
+        _propFR.SetFloat("Speed", slowSpeed);
+        _propRL.SetFloat("Speed", slowSpeed);
+        _propRR.SetFloat("Speed", slowSpeed);
     }
 
     void OnTakeOff(object sender, EventArgs e)
@@ -53,42 +61,205 @@ public class AliaAnimationManager : MonoBehaviour
         if (!_aliaMovement.IsLanded)
         {
             float vertical = Input.GetAxisRaw("Vertical");
+            //float vertical = -_aliaRigidbody.gameObject.transform.rotation.eulerAngles.x;
             float horizontal = Input.GetAxisRaw("Horizontal");
-            if(vertical > 0.1)
+            //float horizontal = _aliaRigidbody.gameObject.transform.rotation.eulerAngles.z;
+            float turn = Input.GetAxisRaw("Turn");
+
+            if (vertical > inputThreshold || vertical < -inputThreshold)
             {
-                _propFL.SetFloat("Speed", forwardSpeed);
-                _propFR.SetFloat("Speed", forwardSpeed);
-                _propRL.SetFloat("Speed", speed);
-                _propRR.SetFloat("Speed", speed);
+                //Pitch
+                if(vertical > inputThreshold)
+                {
+                    //Pitch Forward
+                    _propFL.SetFloat("Speed", forwardSpeed);
+                    _propFR.SetFloat("Speed", forwardSpeed);
+                    _propRL.SetFloat("Speed", speed);
+                    _propRR.SetFloat("Speed", speed);
+                    
+                    if (horizontal > inputThreshold || horizontal < -inputThreshold)
+                    {
+                        //Pitch and Roll Forward
+                        if(horizontal > inputThreshold)
+                        {
+                            //Pitch and Roll Forward Right
+                            PitchAndRollForwardRight();
+                        }
+                        else if (horizontal < -inputThreshold)
+                        {
+                            //Pitch and Roll Forward Left
+                            PitchAndRollForwardLeft();
+                        }
+                    }
+                
+                    if (turn > inputThreshold || turn < -inputThreshold)
+                    {
+                        //Pitch and Yaw Forward
+                        if(turn > inputThreshold)
+                        {
+                            //Pitch and Yaw Forward Clockwise
+                            PitchAndRollForwardRight();
+
+                        }
+                        else if (turn < -inputThreshold)
+                        {
+                            //Pitch and Yaw Forward Counter-Clockwise
+                            PitchAndRollForwardLeft();
+
+                        }
+                    }
+                }
+                else if (vertical < -inputThreshold)
+                {
+                    //Pitch backward
+                    _propFL.SetFloat("Speed", speed);
+                    _propFR.SetFloat("Speed", speed);
+                    _propRL.SetFloat("Speed", forwardSpeed);
+                    _propRR.SetFloat("Speed", forwardSpeed);
+                    
+                    if (horizontal > inputThreshold || horizontal < -inputThreshold)
+                    {
+                        //Pitch and Roll backward
+                        if(horizontal > inputThreshold)
+                        {
+                            //Pitch and Roll backward Right
+                            PitchAndRollBackwardRight();
+                        }
+                        else if (horizontal < -inputThreshold)
+                        {
+                            //Pitch and Roll backward Left
+                            PitchAndRollBackwardLeft();
+                        }
+                    }
+                    
+                    if (turn > inputThreshold || turn < -inputThreshold)
+                    {
+                        //Pitch and Yaw backward
+                        if(turn > inputThreshold)
+                        {
+                            //Pitch and Yaw backward Clockwise
+                            PitchAndRollBackwardLeft();
+
+                            
+                        }
+                        else if (turn < -inputThreshold)
+                        {
+                            //Pitch and Yaw backward Counter-Clockwise
+                            PitchAndRollBackwardRight();
+                        }
+                    }
+                }
             }
-            else if (vertical < -0.1)
+            else if (horizontal > inputThreshold || horizontal < -inputThreshold)
             {
-                _propFL.SetFloat("Speed", speed);
-                _propFR.SetFloat("Speed", speed);
-                _propRL.SetFloat("Speed", forwardSpeed);
-                _propRR.SetFloat("Speed", forwardSpeed);
+                //Roll
+                if(horizontal > inputThreshold)
+                {
+                    //Roll Right
+                    _propFL.SetFloat("Speed", speed);
+                    _propFR.SetFloat("Speed", forwardSpeed);
+                    _propRL.SetFloat("Speed", speed);
+                    _propRR.SetFloat("Speed", forwardSpeed);
+
+                    if (turn > inputThreshold || turn < -inputThreshold)
+                    {
+                        //Roll and Yaw Right
+                        if(turn > inputThreshold)
+                        {
+                            //Roll and Yaw Right Clockwise
+                            PitchAndRollBackwardRight();
+                        }
+                        else if (turn < -inputThreshold)
+                        {
+                            //Roll and Yaw Right Counter-Clockwise
+                            PitchAndRollForwardRight();
+                        }
+                    }
+                }
+                else if (horizontal < -inputThreshold)
+                {
+                    //Roll Left
+                    _propFL.SetFloat("Speed", forwardSpeed);
+                    _propFR.SetFloat("Speed", speed);
+                    _propRL.SetFloat("Speed", forwardSpeed);
+                    _propRR.SetFloat("Speed", speed);
+                    
+                    if (turn > inputThreshold || turn < -inputThreshold)
+                    {
+                        //Roll and Yaw Left
+                        if(turn > inputThreshold)
+                        {
+                            //Roll and Yaw Left Clockwise
+                            PitchAndRollForwardLeft();
+                        }
+                        else if (turn < -inputThreshold)
+                        {
+                            //Roll and Yaw Left Counter-Clockwise
+                            PitchAndRollBackwardLeft();
+                        }
+                    }
+                }
             }
-            else if(horizontal > 0.1)
+            else if (turn > inputThreshold || turn < -inputThreshold)
             {
-                _propFL.SetFloat("Speed", speed);
-                _propFR.SetFloat("Speed", forwardSpeed);
-                _propRL.SetFloat("Speed", speed);
-                _propRR.SetFloat("Speed", forwardSpeed);
-            }
-            else if (horizontal < -0.1)
-            {
-                _propFL.SetFloat("Speed", forwardSpeed);
-                _propFR.SetFloat("Speed", speed);
-                _propRL.SetFloat("Speed", forwardSpeed);
-                _propRR.SetFloat("Speed", speed);
+                //Yaw
+                if(turn > inputThreshold)
+                {
+                    //Yaw Clockwise
+                    _propFL.SetFloat("Speed", speed);
+                    _propFR.SetFloat("Speed", forwardSpeed);
+                    _propRL.SetFloat("Speed", forwardSpeed);
+                    _propRR.SetFloat("Speed", speed);
+                }
+                else if (turn < -inputThreshold)
+                {
+                    //Yaw Counter-Clockwise
+                    _propFL.SetFloat("Speed", forwardSpeed);
+                    _propFR.SetFloat("Speed", speed);
+                    _propRL.SetFloat("Speed", speed);
+                    _propRR.SetFloat("Speed", forwardSpeed);
+                }
             }
             else
             {
+                //hover
                 _propFL.SetFloat("Speed", speed);
                 _propFR.SetFloat("Speed", speed);
                 _propRL.SetFloat("Speed", speed);
                 _propRR.SetFloat("Speed", speed);
             }
         }
+    }
+
+    private void PitchAndRollBackwardLeft()
+    {
+        _propFL.SetFloat("Speed", mediumSpeed);
+        _propFR.SetFloat("Speed", speed);
+        _propRL.SetFloat("Speed", forwardSpeed);
+        _propRR.SetFloat("Speed", mediumSpeed);
+    }
+
+    private void PitchAndRollBackwardRight()
+    {
+        _propFL.SetFloat("Speed", speed);
+        _propFR.SetFloat("Speed", mediumSpeed);
+        _propRL.SetFloat("Speed", mediumSpeed);
+        _propRR.SetFloat("Speed", forwardSpeed);
+    }
+
+    private void PitchAndRollForwardLeft()
+    {
+        _propFL.SetFloat("Speed", forwardSpeed);
+        _propFR.SetFloat("Speed", mediumSpeed);
+        _propRL.SetFloat("Speed", mediumSpeed);
+        _propRR.SetFloat("Speed", speed);
+    }
+
+    private void PitchAndRollForwardRight()
+    {
+        _propFL.SetFloat("Speed", mediumSpeed);
+        _propFR.SetFloat("Speed", forwardSpeed);
+        _propRL.SetFloat("Speed", speed);
+        _propRR.SetFloat("Speed", mediumSpeed);
     }
 }
